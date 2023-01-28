6d7924a6e287f7d806443461024e0dd5e8639de6
ArturSydor <53897689+ArturSydor@users.noreply.github.com>
2023-01-28 21:10:40 +0100

    updated data generator

--- sensors-data-generator/generator.py
+++ sensors-data-generator/generator.py
@@ -3,10 +3,16 @@ import json
 import os
 import random
 import time
+from enum import Enum
 
 import paho.mqtt.client as mqtt
 
 one_year_in_milliseconds = 31_556_952_000
+
+
+class RunMode(Enum):
+    STREAM = "STREAM"
+    ITERATION = "ITERATION"
 
 
 def random_decimal_string(min_value: int, max_value: int):
@@ -65,7 +71,7 @@ def get_air_quality_data():
     return data
 
 
-def publish_message(topic: str, data_supplier):
+def publish_message(client, topic: str, data_supplier):
     data = data_supplier()
 
     data_out = json.dumps(data)
@@ -75,30 +81,91 @@ def publish_message(topic: str, data_supplier):
     print(result)
 
 
-broker = os.getenv("BROKER_HOST")
-port = int(os.getenv("BROKER_PORT"))
-username = os.getenv("BROKER_USER")
-password = os.getenv("BROKER_PASSWORD")
+def create_client():
+    broker = os.getenv("BROKER_HOST")
+    port: int = int(os.getenv("BROKER_PORT"))
+    username = os.getenv("BROKER_USER")
+    password = os.getenv("BROKER_PASSWORD")
 
-client = mqtt.Client()
-client.username_pw_set(username, password)
-is_connected = False
-while not is_connected:
-    try:
-        client.connect(broker, port)
-        is_connected = True
-    except Exception:
-        print("MQTT broker is not available yet")
-        time.sleep(1)
+    client = mqtt.Client()
+    client.username_pw_set(username, password)
 
-iterations_count = random.randint(1, 10_000)
-for i in range(iterations_count):
-    sleep_duration = random.randint(0, 1)
+    is_connected = False
+    while not is_connected:
+        try:
+            client.connect(broker, port)
+            is_connected = True
+        except Exception:
+            print("MQTT broker is not available yet")
+            time.sleep(1)
+    return client
 
-    publish_message("building/floor_1/bedroom/water_quality", get_water_quality_data)
-    publish_message("building/weather", get_weather_data)
-    publish_message("building/001/air_quality", get_air_quality_data)
 
-    time.sleep(sleep_duration)
-    client.loop()
-client.disconnect()
+def generate_sensor_data():
+    run_mode: RunMode = RunMode(os.getenv("RUN_MODE"))
+    number_of_iterations: int = int(os.getenv("NUMBER_OF_ITERATIONS"))
+    water_quality_topic = os.getenv("WATER_QUALITY_TOPIC")
+    weather_topic = os.getenv("WEATHER_TOPIC")
+    air_quality_topic = os.getenv("AIR_QUALITY_TOPIC")
+
+    client = create_client()
+
+    if run_mode == RunMode.STREAM:
+        while True:
+            publish_message(client, water_quality_topic, get_water_quality_data)
+            time.sleep(1)
+
+            publish_message(client, weather_topic, get_weather_data)
+            time.sleep(1)
+
+            publish_message(client, air_quality_topic, get_air_quality_data)
+            time.sleep(1)
+
+            client.loop()
+    elif run_mode == RunMode.ITERATION:
+        for i in range(number_of_iterations):
+            publish_message(client, water_quality_topic, get_water_quality_data)
+            publish_message(client, weather_topic, get_weather_data)
+            publish_message(client, air_quality_topic, get_air_quality_data)
+
+            time.sleep(1)
+            client.loop()
+    else:
+        print("Not existing run mode: " + run_mode.__str__())
+    client.disconnect()
+
+
+generate_sensor_data()
+
+# broker = os.getenv("BROKER_HOST")
+# port: int = int(os.getenv("BROKER_PORT"))
+# username = os.getenv("BROKER_USER")
+# password = os.getenv("BROKER_PASSWORD")
+# run_mode: RunMode = RunMode(os.getenv("RUN_MODE"))
+# number_of_iterations: int = int(os.getenv("NUMBER_OF_ITERATIONS"))
+# water_quality_topic = os.getenv("WATER_QUALITY_TOPIC")
+# weather_topic = os.getenv("WEATHER_TOPIC")
+# air_quality_topic = os.getenv("AIR_QUALITY_TOPIC")
+#
+# client = mqtt.Client()
+# client.username_pw_set(username, password)
+# is_connected = False
+# while not is_connected:
+#     try:
+#         client.connect(broker, port)
+#         is_connected = True
+#     except Exception:
+#         print("MQTT broker is not available yet")
+#         time.sleep(1)
+#
+# iterations_count = random.randint(1, 10_000)
+# for i in range(iterations_count):
+#     sleep_duration = random.randint(0, 1)
+#
+#     publish_message("building/floor_1/bedroom/water_quality", get_water_quality_data)
+#     publish_message("building/weather", get_weather_data)
+#     publish_message("building/001/air_quality", get_air_quality_data)
+#
+#     time.sleep(sleep_duration)
+#     client.loop()
+# client.disconnect()
